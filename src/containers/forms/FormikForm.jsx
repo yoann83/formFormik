fb735e9e778a4452e85e7007bfda481576e4bdd3
Martinez yoann <41195157+yoann83@users.noreply.github.com>
2022-06-17 14:50:34 +0200

    update project date

--- src/containers/forms/FormikForm.jsx
+++ src/containers/forms/FormikForm.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import * as yup from "yup";
-import Button from "@material-ui/core/Button";
+import { Button } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import { Formik, Form, useFormik } from "formik";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import "./forms.scss";
 
 export default function FormikFOrm() {
-  const title = "formik";
+  const title = "formik & materail-ui";
+  const [therms, isTherms] = React.useState(false);
 
+  //FORM SCHEMA VALIDATOR
   const validationSchema = yup.object({
     lastName: yup.string("Enter your Name").required("LastName is required"),
     email: yup
@@ -29,11 +34,25 @@ export default function FormikFOrm() {
         then: yup
           .string()
           .oneOf([yup.ref("password")], "Both password need to be the same !")
-      })
+      }),
+    date: yup.string().required("Select Date"),
+    is_therms: yup.string().required("Select")
   });
 
+  const getThermsValue = (value) => {
+    value ? isTherms(false) : isTherms(true);
+  };
+
   const formik = useFormik({
-    initialValues: {},
+    initialValues: {
+      lastName: "",
+      firstName: "",
+      email: "",
+      password: "",
+      confimPassword: "",
+      date: "",
+      is_therms: false
+    },
     validationSchema: validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
@@ -42,14 +61,14 @@ export default function FormikFOrm() {
 
   return (
     <section>
-      <div>
+      <div className="form-formik-material-ui">
         <h1>{title.toLocaleUpperCase()}</h1>
         <Formik
-          initialValues={{}}
           onSubmit={async (values) => {
             await new Promise((r) => setTimeout(r, 500));
             alert(JSON.stringify(values, null, 2));
           }}
+          initialValues={undefined}
         >
           <Form onSubmit={formik.handleSubmit}>
             <TextField
@@ -63,9 +82,12 @@ export default function FormikFOrm() {
                 formik.touched.firstName && Boolean(formik.errors.firstName)
               }
               helperText={formik.touched.firstName && formik.errors.firstName}
+              style={{ margin: "1em 0" }}
+              InputLabelProps={{ style: { color: "#274058" } }}
             />
             <TextField
               fullWidth
+              required
               id="lastName"
               name="lastName"
               label="LastName"
@@ -73,9 +95,27 @@ export default function FormikFOrm() {
               onChange={formik.handleChange}
               error={formik.touched.lastName && Boolean(formik.errors.lastName)}
               helperText={formik.touched.lastName && formik.errors.lastName}
+              style={{ margin: "1em 0" }}
             />
             <TextField
               fullWidth
+              required
+              name="date"
+              value={formik.values.date}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              helperText={formik.touched.date ? formik.errors.date : ""}
+              error={formik.touched.date && Boolean(formik.errors.date)}
+              label="Date of Birth"
+              type="datetime-local"
+              InputLabelProps={{
+                shrink: true
+              }}
+              style={{ margin: "1em 0" }}
+            />
+            <TextField
+              fullWidth
+              required
               id="email"
               name="email"
               label="Email"
@@ -83,9 +123,11 @@ export default function FormikFOrm() {
               onChange={formik.handleChange}
               error={formik.touched.email && Boolean(formik.errors.email)}
               helperText={formik.touched.email && formik.errors.email}
+              style={{ margin: "1em 0" }}
             />
             <TextField
               fullWidth
+              required
               id="password"
               name="password"
               label="Password"
@@ -93,9 +135,11 @@ export default function FormikFOrm() {
               onChange={formik.handleChange}
               error={formik.touched.password && Boolean(formik.errors.password)}
               helperText={formik.touched.password && formik.errors.password}
+              style={{ margin: "1em 0" }}
             />
             <TextField
               fullWidth
+              required
               id="confimPassword"
               name="confimPassword"
               label="Confirm password"
@@ -108,9 +152,31 @@ export default function FormikFOrm() {
               helperText={
                 formik.touched.confimPassword && formik.errors.confimPassword
               }
+              style={{ margin: "1em 0" }}
             />
-            <Button color="primary" variant="contained" fullWidth type="submit">
-              Submit
+            <FormControlLabel
+              name="is_therms"
+              value={formik.values.is_therms}
+              checked={formik.values.is_therms}
+              onChange={formik.handleChange}
+              onClick={() => getThermsValue(formik.values.is_therms)}
+              onBlur={formik.handleBlur}
+              control={<Switch color="primary" />}
+              label="Accept therms of use"
+              labelPlacement="end"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                padding: "1em"
+              }}
+            />
+            <Button
+              color="primary"
+              disabled={!therms}
+              variant="contained"
+              type="submit"
+            >
+              Register
             </Button>
           </Form>
         </Formik>
